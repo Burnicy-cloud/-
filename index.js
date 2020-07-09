bda5df953528794efa3112317af3617e850613b7
weihaiyu <1600219867@qq.com>
2020-07-09 21:36:52 +0800

    提交

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ console.log("初始化");
 console.log("一些逻辑代码");
 console.log("第二次提交代码");
 // git add .  添加到暂存区  git commit -m :提交仓库； git status 查看状态； git log 查看提交日志（git log --oneline /git reflog）;
-console.log("第三次添加的内容"); // git add . ---> git commit -m "提交"   简化  git commit -a -m "提交到本地仓库"
+console.log("第三次添加的内容"); // git add .(所有修改的添加文件) ---> git commit -m "提交"   简化  git commit -a -m "提交到本地仓库"
 console.log("提交代码");
+
+// 删除 工作区 仓库 文件  git -rm 文件名；
