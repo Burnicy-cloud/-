0abe5690608fc6250828884ef5503149cb29aa9b
weihaiyu <1600219867@qq.com>
2020-07-09 21:30:33 +0800

    提交到本地仓库

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("初始化");
 console.log("一些逻辑代码");
 console.log("第二次提交代码");
 // git add .  添加到暂存区  git commit -m :提交仓库； git status 查看状态； git log 查看提交日志（git log --oneline /git reflog）;
+console.log("第三次添加的内容"); // git add . ---> git commit -m "提交"   简化  git commit -a -m "提交到本地仓库"
